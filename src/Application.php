e4551a82873aaf5d1aab31b42991e5fbfa7d867a
Weng Long Pang (WE-EAQ) <weng.pang@gmail.com>
2019-01-26 20:38:48 +1100

    Change of CSRF Protection behaviour Previously the CSRF is applied to the whole application (see Application.php)
    This change will allow RESTful request not to compile the CSRF requirement.

--- src/Application.php
+++ src/Application.php
@@ -78,12 +78,22 @@ class Application extends BaseApplication
             // pass null as cacheConfig, example: `new RoutingMiddleware($this)`
             // you might want to disable this cache in case your routing is extremely simple
             ->add(new RoutingMiddleware($this, '_cake_routes_'))
-
-            // Add csrf middleware.
-            ->add(new CsrfProtectionMiddleware([
-                'httpOnly' => true
-            ]));
+        ;
 
         return $middlewareQueue;
+    }
+
+    /**
+     * Prepare specific settings for Routes
+     *
+     * @param \Cake\Routing\RouteBuilder $routes
+     */
+    public function routes($routes)
+    {
+        // CSRF
+        $options = [
+            'httpOnly' => true,
+        ];
+        $routes->registerMiddleware('csrf', new CsrfProtectionMiddleware($options));
     }
 }
