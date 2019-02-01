37af73ee507f71c21d1854e02dc24603db893368
Weng Long Pang (WE-EAQ) <weng.pang@gmail.com>
2019-02-01 23:32:19 +1100

    Reset Command code is ready for use
    Error handling is pending

--- src/Command/DatabaseResetCommand.php
+++ src/Command/DatabaseResetCommand.php
@@ -24,52 +24,47 @@ class DatabaseResetCommand extends Command
         return $parser;
     }
 
-
     public function execute(Arguments $args, ConsoleIo $io)
     {
         $connection = ConnectionManager::get('default'); // TODO DRY
-        $db = $connection->config()['database'];
         $config = $connection->config();
         $fileList = array();
+        $nameList = array();
         // Find all relevant files from the directory
         // https://stackoverflow.com/questions/2922954/getting-the-names-of-all-files-in-a-directory-with-php
-        foreach(glob(BACKUP_SCHEMA_DIR . $config['file_prefix'] . DOT . '*' . DOT . $config['file_type']) as $file){
+        foreach(glob( BACKUP_SCHEMA_DIR . $config['file_prefix'] . DOT . '*' . DOT . $config['file_type']) as $file){
             $fileList[] = $file;
+            $allNames = explode( DS , $file);
+            $nameList[] = $allNames[sizeof($allNames) - 1];
         }
-        debug(array_keys($fileList));
+
         if (sizeof($fileList) == 0) {
             $io->out('No backup files found. Aborting');
             die; // Exit the command if no files found
         }
         // Proceed to file selection
         do {
-            $waitingForEntry = false;
+            $waitingForEntry = true;
             $io->out('Please select one of the backup file');
-            $response = $io->askChoice('Type in the number and press enter',array_keys($fileList));
+            print_r(($nameList));
+            $response = $io->askChoice('Type in the number and press enter OR press n to leave:',array_keys($fileList));
             // Input Validation
-
+            if(strtolower($response) == 'n'){
+                die;
+            } elseif (!array_key_exists($response,$fileList)){
+                $io->error('The Key entered is invalid, please try again');
+            } else {
+                $waitingForEntry = false;
+            }
         } while ($waitingForEntry);
         $io->out("Dropping existing database");
         // Switching off referential integrity
         $connection->prepare("SET FOREIGN_KEY_CHECKS = 0;")->execute();
-//        $connection->prepare("DROP DATABASE {$db}")->execute();
-//
-//        $this->out("Recreating new database");
-//        $connection->prepare("CREATE DATABASE {$db}")->execute();
-//        $connection->prepare("USE {$db}")->execute();
-
         $io->out("Apply schema and data into the database");
-//        $script = (new File(SCHEMA_DIR . $config['file_prefix'] .$config['file_type']))->read(); //TODO Add multiple file support
-//        $connection->prepare($script)->execute();
-
+        $script = (new File($fileList[$response]))->read();
+        $connection->prepare($script)->execute();
         // Switching on referential integrity
         $connection->prepare("SET FOREIGN_KEY_CHECKS = 1;")->execute();
-
         $io->success("Done!");
-    }
-
-    public function outputHelp()
-    {
-        $this->out($this->OptionParser->help());
     }
 }
